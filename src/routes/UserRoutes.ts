c41c7950ca6ecebfbb40fb9b6c9fece931a054c4
talis1999 <dantalisman@gmail.com>
2023-11-28 23:51:56 +0200

    fixing express import

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import * as express from "express";
 
 import accountRoutes from "./AccountRoutes";
 import { createNewUser, getUserById } from "../controllers/UserController";
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import * as express from "express";
+
 import { AppDataSource } from "./db";
 import { User } from "./entity/User";
+import userRoutes from "./routes/UserRoutes";
 
 AppDataSource.initialize()
   .then(async () => {
@@ -26,5 +28,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/users", userRoutes);
 
 export default app;
