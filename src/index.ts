6002d931579b19134da64bde21238ffd0787c1fe
talis1999 <dantalisman@gmail.com>
2023-11-28 23:10:30 +0200

    moving content to server.ts

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,6 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import { Request, Response } from "express";
-import { AppDataSource } from "./data-source";
-import { Routes } from "./routes";
-import { User } from "./entity/User";
+import app from "./server";
 
-AppDataSource.initialize()
-  .then(async () => {
-    await AppDataSource.manager.save(
-      AppDataSource.manager.create(User, {
-        name: "Karin",
-        document: "",
-        birthDate: new Date(),
-      })
-    );
-
-    await AppDataSource.manager.save(
-      AppDataSource.manager.create(User, {
-        name: "Sans",
-        document: "",
-        birthDate: new Date(),
-      })
-    );
-  })
-  .catch((error) => console.log(error));
-
-const app = express();
-app.use(bodyParser.json());
-
-app.listen(3000);
+const port: number = Number(process.env.PORT || 3000);
+app.listen(port, () => {
+  console.log("Express server started on port: " + port);
+});
