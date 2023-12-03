5c988b5b6e0d07da4d7df39de1ddd729cd14ad6f
talis1999 <dantalisman@gmail.com>
2023-12-03 05:48:32 +0200

    updating joi validation

--- src/middleware/joiValidation.ts
+++ src/middleware/joiValidation.ts
@@ -2,24 +2,16 @@ import { Request, Response, NextFunction } from "express";
 import * as Joi from "joi";
 import * as _ from "lodash";
 
-export const queryValidation =
+export enum RequestKeys {
+  Query = "query",
+  Body = "body",
+}
+
+export const joiValidation =
+  (key: RequestKeys) =>
   (schema: Joi.ObjectSchema) =>
   (req: Request, res: Response, next: NextFunction) => {
-    const validationResult = schema.validate(req.query);
-
-    if (validationResult.error) {
-      return res
-        .status(400)
-        .json({ error: validationResult.error?.details[0]?.message });
-    }
-
-    next();
-  };
-
-export const bodyValidation =
-  (schema: Joi.ObjectSchema) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    const validationResult = schema.validate(req.body);
+    const validationResult = schema.validate(_.get(req, key));
 
     if (validationResult.error) {
       return res
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -2,14 +2,14 @@ import * as express from "express";
 
 import { login, logout, register } from "../controllers/UserController";
 import { authenticateToken } from "../middleware/authenticateToken";
-import { bodyValidation } from "../middleware/joiValidation";
+import { RequestKeys, joiValidation } from "../middleware/joiValidation";
 import { loginSchema, userSchema } from "../joi_schemas/User";
 
 const router = express.Router();
 
-router.post("/register", bodyValidation(userSchema), register);
+router.post("/register", joiValidation(RequestKeys.Body)(userSchema), register);
 
-router.post("/login", bodyValidation(loginSchema), login);
+router.post("/login", joiValidation(RequestKeys.Body)(loginSchema), login);
 
 router.post("/logout", authenticateToken, logout);
 
